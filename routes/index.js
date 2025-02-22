0427904a6046839270b49c4f1b55d3d11f28be4f
lin yen <s920201lin87@gmail.com>
2025-02-22 09:28:11 +0800

    2025/02/22 09:28 update style.css add  app.post('/account)in index.js

--- routes/index.js
+++ routes/index.js
@@ -8,5 +8,9 @@ router.get('/account', function(req, res, next) {
 router.get('/account/create',(req,res,next)=>{
   res.render('create')
 })
-
+router.post('/account',(req,res)=>{
+  console.log(req.body);
+  console.log(req.header)
+  res.send('加入紀錄');
+});
 module.exports = router;
