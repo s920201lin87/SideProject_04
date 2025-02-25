39972451e464173afc78d59b5f11660ead43ec0d
Lin Yen <s920201lin87@gmail.com>
2025-02-26 01:28:18 +0800

    2025/02/26 13:28 update callback to promise /api/account.js

--- routes/api/account.js
+++ routes/api/account.js
@@ -1,136 +1,111 @@
 const express = require("express");
 const router = express.Router();
-
 const moment = require("moment");
 const AccountModel = require("../../models/AccountModel");
 
-// console.log(moment(new Date()).format("YYYY-MM-DD"));
-
-//list
-router.get("/account", function (req, res, next) {
-    // let accounts= db.get('accounts').value();
-    AccountModel.find()
-        .sort({ time: -1 })
-        .exec((err, data) => {
-            if (err) {
-                // res.status(500).send("讀取失敗");
-                res.status(500).json({
-                    code:"1001",
-                    msg:"讀取失敗",
-                    data:null
-                })
-                return;
-            }
-            console.log(data);
-            res.json({
-                code:'0000', 
-                msg:'讀取成功',
-                data:data
-            })
+// 取得帳號資料列表
+router.get("/account", async (req, res) => {
+    try {
+        const data = await AccountModel.find().sort({ time: -1 }).exec();
+        res.json({
+            code: "0000",
+            msg: "讀取成功",
+            data: data
         });
+    } catch (err) {
+        res.status(500).json({
+            code: "1001",
+            msg: "讀取失敗",
+            data: null
+        });
+    }
 });
 
-//add
-router.post("/account", (req, res) => {
-    // let id = shortid.generate();
-    // db.get('accounts').unshift({id:id,...req.body}).write();
-    // console.log(req.body)
-    //插入資料庫
-    AccountModel.create(
-        {
+// 新增帳號資料
+router.post("/account", async (req, res) => {
+    try {
+        const data = await AccountModel.create({
             ...req.body,
             time: moment(req.body.time).toDate(),
-        },
-        (err, data) => {
-            if (err) {
-                res.status(500).json({
-                    code:"1002",
-                    msg:"新增錯誤",
-                    data:null
-                })
-                return;
-            }
-            res.status(200).json({
-                code:"0000",
-                msg:"新增成功",
-                data:data     
-            })
-        }
-    );
+        });
+        res.status(200).json({
+            code: "0000",
+            msg: "新增成功",
+            data: data
+        });
+    } catch (err) {
+        res.status(500).json({
+            code: "1002",
+            msg: "新增錯誤",
+            data: null
+        });
+    }
 });
 
-//delete id
-router.delete("/account/:id", (req, res) => {
-    let id = req.params.id;
-    // db.get('accounts').remove({id:id}).write();
-    AccountModel.deleteOne({ _id: id }, (err, data) => {
-        if (err) {
-            res.status(500).json({
-                code:"1003",
-                msg:"刪除失敗",
-                data:null
-            })
-            return;
-        }
+// 刪除指定 id 的帳號資料
+router.delete("/account/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+        await AccountModel.deleteOne({ _id: id });
         res.status(200).json({
-            code:"0000",
-            msg:"刪除成功",
-            data:{}
-        })
-    });
+            code: "0000",
+            msg: "刪除成功",
+            data: {}
+        });
+    } catch (err) {
+        res.status(500).json({
+            code: "1003",
+            msg: "刪除失敗",
+            data: null
+        });
+    }
 });
 
-//find id 
-router.get('/account/:id',(req,res) => {
-    let {id} = req.params;
-    AccountModel.findById(id,(err,data)=>{
-        if(err){
-            return res.status(500).json({
-                code:"1004",
-                msg:"讀取失敗",
-                data:null
-            })
-            
+// 取得指定 id 的帳號資料
+router.get("/account/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+        const data = await AccountModel.findById(id).exec();
+        if (!data) {
+            return res.status(404).json({
+                code: "1004",
+                msg: "找不到該筆資料",
+                data: null
+            });
         }
-        
         res.status(200).json({
-            code:"0000",
-            msg:"成功讀取",
-            data:data
-        })
-    })
-})
+            code: "0000",
+            msg: "成功讀取",
+            data: data
+        });
+    } catch (err) {
+        res.status(500).json({
+            code: "1004",
+            msg: "讀取失敗",
+            data: null
+        });
+    }
+});
 
-//update
-router.patch("/account/:id",(req,res) => {
-    let {id} = req.params;
-    AccountModel.updateOne({_id: id},req.body,(err,data)=>{
-        if(err){
-            return res.status(500).json({
-                code:"1005",
-                msg:"更新失敗",
-                data:null
-            })
-        }
-        AccountModel.findById(id,(err,data)=>{
-            if(err){
-                return res.status(500).json({
-                    code:"1004",
-                    msg:"讀取失敗",
-                    data:null
-                })
-                
-            }
-            res.status(200).json({
-                code:"0000",
-                msg:"成功更新",
-                data:data
-            })
-
-        })
-        
-    })
-})
-
+// 更新指定 id 的帳號資料
+router.patch("/account/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+        await AccountModel.updateOne({ _id: id }, req.body);
+        // 更新後再查詢一次，回傳最新資料
+        const updatedData = await AccountModel.findById(id).exec();
+        res.status(200).json({
+            code: "0000",
+            msg: "成功更新",
+            data: updatedData
+        });
+    } catch (err) {
+        res.status(500).json({
+            code: "1005",
+            msg: "更新失敗",
+            data: null
+        });
+    }
+});
 
 module.exports = router;
